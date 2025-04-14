931780194e175cab707af7eff30553be40b3e7ee
Trunghoang21 <trunghoang2179@gmail.com>
2025-04-14 10:11:26 +0200

    Linear Probing done

--- 2hashtables/LinearProbingHashTable.py
+++ 2hashtables/LinearProbingHashTable.py
@@ -4,40 +4,121 @@ class LinearProbingHashTable:
     def __init__(self, size=1):
         """ Initialize the hash table """
         self.size = size
-        self.table = [(None, None)] * size  # (key, value) pairs or (None, None) for empty
-        self.tombstone = (object(), None)  # Special marker for deleted elements
+        self.table = [None] * size  # Using None to indicate empty slots
         self.count = 0
-        self.max_load_factor = 0.5  # Lower than separate chaining
-        self.min_load_factor = 0.125
+        self.max_load_factor = 0.75  # Lower than separate chaining
+
+
         
     def _hash(self, key):
         """ Hash the key to an index in the table """
         return hash(key) % self.size
+
+        #hash_value = 5381
+        #for c in key:
+        #    hash_value = ((hash_value << 5) + hash_value) + ord(c)
+        #return hash_value % self.size
         
     def _resize(self):
         """ Resize the hash table if load factor is exceeded """
-        # Implementation here
+        load_factor = self.count / self.size
+        if load_factor > self.max_load_factor:
+            old_size = self.size # Uncomment when implementing resizing
+            old_table = self.table
+            self.size *= 2
+            self.table = [None] * self.size
+            self.count = 0  # Reset count to reinsert elements
+            for element in old_table: 
+                if element is not None: 
+                    key, value = element
+                    self._reinsert(key,value)
+            #print (f"changed size from {old_size} to {self.size}") # to comment out later 
+            return True
+        return False
+    
+    def _reinsert(self, key, value):
+        """ Reinsert a key-value pair into the hash table"""
+        index= self._hash(key)
+        while self.table[index] is not None:
+            index = (index + 1) % self.size
+        self.table[index] = (key, value)
+        self.count += 1
         
     def insert(self, key):
         """ Insert a key-value pair into the hash table """
-        # Implementation here
+        index = self._hash(key)
+        if  self.table[index] is None:
+            self.table[index] = (key,1)
+            self.count += 1
+            self._resize()
+            return True
+        if self.table[index][0] == key:
+            # update the value of the key, creating new tuple
+            self.table[index] = (key, self.table[index][1] + 1)
+            return True
+        # handle collisions using linear probing
+        while self.table[index] is not None:
+            index = (index + 1) % self.size # Linear probing
+            if self.table[index] is not None and self.table[index][0] == key:
+                # update the value of the key, creating new tuple
+                self.table[index] = (key, self.table[index][1] + 1)
+                return True
+        self.table[index] = (key,1)
+        self.count += 1
+        self._resize()
+        return True
+
+        
         
     def search(self, key):
         """ Search for a key in the hash table """
-        # Implementation here
+        # counter implented for prevting the infinite loops. 
+        counter = 1
+        index = self._hash(key)
+        while self.table[index] is not None and counter < self.size:
+            if self.table[index][0] == key:
+                return True
+            #if self.table[index] is None:
+            #    return False
+            counter += 1
+            index = (index + 1) % self.size
+        return False
+
         
     def remove(self, key):
         """ Remove a key from the hash table """
         # Implementation here
+        index = self._hash(key)
+    
+        while self.table[index] is not None:
+            if self.table[index][0] == key:
+                # Delete the key by setting the slot to None
+                self.table[index] = None
+                self.count -= 1
+
+                # Start backward shifting
+                j = index
+                while True:
+                    index = (index + 1) % self.size
+                    if self.table[index] is None:
+                        return  # Done shifting
+                    k = self._hash(self.table[index][0])
+                    # Check if this entry should be moved back to j
+                    if not (j < k <= index or (index < j < k ) or (k <= index < j)):
+                        self.table[j] = self.table[index]
+                        self.table[index] = None
+                        j = index
+            else:
+                index = (index + 1) % self.size
         
     def __str__(self):
-        return 
+        return str(self.table)
 
 def main():
-    hash_table = SeparateChainingHashTable(1)
+    hash_table = LinearProbingHashTable(1)
     i = 0
 
-    # Word frequency will be tracked here using your hash table
+    # Word frequency will be tracked using LinearProbingHashTable
     for line in sys.stdin:
         word = line.strip()
         remove_it = i % 16 == 0
@@ -53,22 +134,17 @@ def main():
 
     # Find most frequent word and break ties by alphabetical order
     max_count = -1
+    result_word = None
 
-    # Step 1: Find the max count
-    for bucket in hash_table.table:
-        for key, value in bucket:
+    for entry in hash_table.table:
+        if entry is not None:
+            key, value = entry
             if value > max_count:
                 max_count = value
-
-    # Step 2: Among words with max count, pick the smallest in lexicographical order
-    result_word = None
-    for bucket in hash_table.table:
-        for key, value in bucket:
-            if value == max_count:
-                if result_word is None or key < result_word:
+                result_word = key
+            elif value == max_count:
+                if key < result_word:
                     result_word = key
-
-    # Print result
     if result_word is not None:
         print(result_word, max_count)
 
